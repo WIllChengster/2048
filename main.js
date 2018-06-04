0519598d401cfc0c7c4e44cb697c9311ac1e0275
Will Cheng <willchengsters@gmail.com>
2018-06-03 23:28:24 -0700

    starting some functionality

--- main.js
+++ main.js
@@ -5,7 +5,40 @@ function initializeApp(){
 }
 
 function attachEventHandlers(){
-    $('.game-piece').on('click', ()=>{
+    $('.game-piece').on('click', () => {
         console.log('clicked')
-    })
+    });
+    $('.up').on('click', () => {
+        console.log('up')
+    });
+    $('.down').on('click', () => {
+        console.log('down')
+    });
+    $('.left').on('click', () => {
+        console.log('left')
+    });
+    $('.right').on('click',() => {
+        console.log('right')
+    });
 }
+
+class App {
+    constructor(game){
+        this.score=0;
+
+    }
+    initGame(){
+        console.log('initializing game');
+        const gamePiece = $('<div class="game-piece"></div>');
+        const row = $('<div class="row"></div>');
+        row.append(gamePiece)
+        $('.game-board').append(row)
+    }
+    restartGame(){
+        console.log('restarting game');
+    }
+
+}
+const Game = new App
+Game.initGame()
+
