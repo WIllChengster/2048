28ab37bad553fc294d5c64e9a322fceaba738d23
Will Cheng <willchengsters@gmail.com>
2018-06-04 18:23:52 -0700

    game start functionality --> game generates '2' at a random location

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 $(document).ready(initializeApp)
 
 function initializeApp(){
+    Game.restartGame()
     attachEventHandlers()
-    const Game = new App
-
-    Game.initGame()
-
 }
 
 function attachEventHandlers(){
@@ -29,29 +26,61 @@ function attachEventHandlers(){
 class App {
     constructor(game){
         this.score=0;
-
+        this.array=[
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null],
+        ]
     }
-    initGame(){
+
+    newBoard(){
+        $('.row').remove();
+        this.array=[
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null],
+            [null, null, null, null],
+        ];
         console.log('initializing game');
         for(let i = 0; i<4; i++){
             const row = $('<div>', {
-                'class': 'row'
+                'class': 'row',
+                'id':`row${i}`
             });
             let gamePiece=null;
             for(let j = 0; j<4; j++){
                  gamePiece = $('<div>',{
-                    'class': 'game-piece'
+                    'class': 'game-piece',
+                    'id':`col${j}`,
+                    'text': null
                 });
-                row.append(gamePiece)
-
+                row.append(gamePiece);
             }
-
-            $('.game-board').append(row)
-        }
-
+            $('.game-board').append(row);
+        };
     }
+
+    startGame(){
+        for(let i = 0; i<2; i++){
+            const randRow = Math.floor(Math.random()*4);
+            const randCol = Math.floor(Math.random()*4);
+            if(!this.array[randRow][randCol]){
+                this.array[randRow][randCol] = 2;
+                $(`#row${randRow} #col${randCol}`).text(2);
+
+            }else {
+                i--;
+            };
+
+
+        };
+    }
+
     restartGame(){
-        console.log('restarting game');
+        this.newBoard();
+        this.startGame();
     }
 
 }
+const Game = new App
